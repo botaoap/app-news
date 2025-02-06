30a6df65dd34fbb5a820297091052f483d588cf3
Gabriel Botao <gabriel.botao@br.experian.com>
2025-02-06 17:47:29 -0300

    implement image view extension

---
+++ app/src/main/java/com/botaoap/appnews/ui/extension/ImageViewExt.kt
@@ -0,0 +1,77 @@
+package com.botaoap.appnews.ui.extension
+
+import android.graphics.drawable.Drawable
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.core.content.ContextCompat
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.botaoap.appnews.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+
+fun ImageView.getImageFromLink(
+    @DrawableRes link: String,
+    imageError: Int,
+    centerRadius: Float = 30f,
+    strokeWidth: Float = 5f,
+    widthLoading: Int = 100,
+    heightLoading: Int = 100,
+    @ColorRes colorLoading: Int = R.color.black,
+) {
+    val view = this
+    val scale = resources.displayMetrics.density
+    val newWidthInPx = (widthLoading * scale).toInt()
+    val newHeightInPx = (heightLoading * scale).toInt()
+
+    view.layoutParams.let {
+        it.width = newWidthInPx
+        it.height = newHeightInPx
+        view.layoutParams = it
+    }
+
+    CircularProgressDrawable(context).let { circularProgressDrawable ->
+        circularProgressDrawable.strokeWidth = strokeWidth
+        circularProgressDrawable.centerRadius = centerRadius
+        circularProgressDrawable.setColorSchemeColors(
+            ContextCompat.getColor(context, colorLoading)
+        )
+        circularProgressDrawable.start()
+
+        Glide.with(context)
+            .load(link)
+            .placeholder(circularProgressDrawable)
+            .error(imageError)
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable,
+                    model: Any,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    view.layoutParams.let {
+                        it.width = ViewGroup.LayoutParams.WRAP_CONTENT
+                        it.height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        view.layoutParams = it
+                    }
+                    return false
+                }
+
+            })
+            .into(this)
+    }
+}
