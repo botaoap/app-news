3a6611dea9b7f16a47618af38b8019f7e8a49893
Gabriel Botao <gabriel.botao@br.experian.com>
2025-02-06 20:16:21 -0300

    create web service

---
+++ app/src/main/java/com/botaoap/appnews/data/remote/api/WebService.kt
@@ -0,0 +1,17 @@
+package com.botaoap.appnews.data.remote.api
+
+import com.botaoap.appnews.data.remote.response.NewsResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WebService {
+
+    /**
+     * @see <a href="https://newsapi.org/docs/endpoints/top-headlines">Documentation</a>
+     */
+    @GET("v2/top-headlines")
+    suspend fun getNews(
+        @Query("sources") sources: String,
+    ) : Response<NewsResponse>
+}
