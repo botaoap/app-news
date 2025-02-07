1647c0fcf09d587bc39595260d19c3f733a5b16c
Gabriel Botao <gabriel.botao@br.experian.com>
2025-02-07 02:08:11 -0300

    update interceptor module

--- app/src/main/java/com/botaoap/appnews/di/InterceptorModule.kt
+++ app/src/main/java/com/botaoap/appnews/di/InterceptorModule.kt
@@ -2,11 +2,13 @@ package com.botaoap.appnews.di
 
 import com.botaoap.appnews.service.HeaderInterceptor
 import com.botaoap.appnews.service.KoinIdentifier
+import okhttp3.Interceptor
 import org.koin.core.qualifier.named
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 object InterceptorModule {
     val modules = module {
-        factory(named(KoinIdentifier.Interceptor.X_API_KEY)) { HeaderInterceptor() }
+        factory(named(KoinIdentifier.Interceptor.X_API_KEY)) { HeaderInterceptor() } bind Interceptor::class
     }
 }
