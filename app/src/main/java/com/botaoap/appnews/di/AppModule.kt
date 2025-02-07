82935d852536b17ca03b55ebcf6b71ce7527f656
Gabriel Botao <gabriel.botao@br.experian.com>
2025-02-07 02:04:25 -0300

    update app module

--- app/src/main/java/com/botaoap/appnews/di/AppModule.kt
+++ app/src/main/java/com/botaoap/appnews/di/AppModule.kt
@@ -8,6 +8,9 @@ import com.botaoap.appnews.domain.repository.Repository
 import com.botaoap.appnews.domain.usecase.GetNewsUseCase
 import com.botaoap.appnews.domain.usecase.GetNewsUseCaseImpl
 import com.botaoap.appnews.service.KoinIdentifier
+import com.botaoap.appnews.ui.feature.newslist.viewmodel.NewsListViewModel
+import kotlinx.coroutines.Dispatchers
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -18,33 +21,40 @@ object AppModule {
         getRepositoryModule(this)
         getUseCaseModule(this)
         getMapperModule(this)
+        getViewModelModule(this)
     }
-}
 
-private fun getWebServiceModule(module: Module) {
-    with(module) {
-        single<WebService> {
-            WebServiceImpl(
-                get(named(KoinIdentifier.Interceptor.X_API_KEY))
-            )
+    private fun getWebServiceModule(module: Module) {
+        with(module) {
+            single<WebService> {
+                WebServiceImpl(
+                    get(named(KoinIdentifier.Interceptor.X_API_KEY))
+                )
+            }
         }
     }
-}
 
-private fun getRepositoryModule(module: Module) {
-    with(module) {
-        single<Repository> { RepositoryImpl(get()) }
+    private fun getRepositoryModule(module: Module) {
+        with(module) {
+            factory<Repository> { RepositoryImpl(get()) }
+        }
     }
-}
 
-private fun getUseCaseModule(module: Module) {
-    with(module) {
-        single<GetNewsUseCase> { GetNewsUseCaseImpl(get(), get(), get()) }
+    private fun getUseCaseModule(module: Module) {
+        with(module) {
+            single<GetNewsUseCase> { GetNewsUseCaseImpl(get(), get(), Dispatchers.IO) }
+        }
     }
-}
 
-private fun getMapperModule(module: Module) {
-    with(module) {
-        single { NewsListMapper() }
+    private fun getMapperModule(module: Module) {
+        with(module) {
+            single { NewsListMapper() }
+        }
+    }
+
+    private fun getViewModelModule(module: Module) {
+        with(module) {
+            viewModel { NewsListViewModel(get()) }
+        }
     }
 }
