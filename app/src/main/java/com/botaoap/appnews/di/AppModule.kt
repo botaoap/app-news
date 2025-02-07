322e33b8ddbf1d66ebca2c1e1f25b708c9b18fe5
Gabriel Botao <gabriel.botao@br.experian.com>
2025-02-06 21:22:52 -0300

    update module

--- app/src/main/java/com/botaoap/appnews/di/AppModule.kt
+++ app/src/main/java/com/botaoap/appnews/di/AppModule.kt
@@ -2,6 +2,11 @@ package com.botaoap.appnews.di
 
 import com.botaoap.appnews.data.remote.api.WebService
 import com.botaoap.appnews.data.remote.network.WebServiceImpl
+import com.botaoap.appnews.data.repository.RepositoryImpl
+import com.botaoap.appnews.domain.mapper.NewsListMapper
+import com.botaoap.appnews.domain.repository.Repository
+import com.botaoap.appnews.domain.usecase.GetNewsUseCase
+import com.botaoap.appnews.domain.usecase.GetNewsUseCaseImpl
 import com.botaoap.appnews.service.KoinIdentifier
 import org.koin.core.module.Module
 import org.koin.core.qualifier.named
@@ -11,6 +16,8 @@ object AppModule {
     val modules = module {
         getWebServiceModule(this)
         getRepositoryModule(this)
+        getUseCaseModule(this)
+        getMapperModule(this)
     }
 }
 
@@ -26,6 +33,18 @@ private fun getWebServiceModule(module: Module) {
 
 private fun getRepositoryModule(module: Module) {
     with(module) {
+        single<Repository> { RepositoryImpl(get()) }
+    }
+}
 
+private fun getUseCaseModule(module: Module) {
+    with(module) {
+        single<GetNewsUseCase> { GetNewsUseCaseImpl(get(), get(), get()) }
+    }
+}
+
+private fun getMapperModule(module: Module) {
+    with(module) {
+        single { NewsListMapper() }
     }
 }
